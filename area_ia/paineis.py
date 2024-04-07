1b7356a5fb71370f46cf0a4b95a5f52b24cd7fc0
Ana Luisa <holtanalu@gmail.com>
2024-04-07 19:48:56 -0300

    inicio da colocacao dos paineis ia

--- area_ia/paineis.py
+++ area_ia/paineis.py
@@ -1,28 +1,51 @@
 from matplotlib import pyplot as plt
 import numpy as np
+import math
 import cv2
 import sys
+
+LARGURA = 2.256  # unidade: m
+ALTURA = 1.133  # unidade: m
 
 cores = [[0, 191, 255], [124, 252, 0], [210, 105, 30], [255, 0, 0], [0, 100, 0], \
          [70, 130, 180], [102, 205, 170], [210, 105, 30], [238, 130, 238], [255, 160, 122]]
 
 
+class Retangulo:
+    def __init__(self, area):
+        self.extremos = []
+        self.angulo = 0
+        self.area = area
+        self.lados = []
+
+
+def metros_para_pixels(valor_metros, lat_cen, zoom):
+    circ_terra = 40075000
+    metros_por_pixel = (np.cos(np.pi*lat_cen/180)*circ_terra)/(np.power(2, 8 + zoom))
+    return valor_metros/metros_por_pixel
+
+
+def magnitude(vector):
+    return math.sqrt(sum(pow(element, 2) for element in vector))
+
+
+# Se for amarelo trasforma em vermelho e verifica o vizinho, se o viziho não for amarelo, nem vermelho, então é uma borda
 def verifica_vizinhos(imagem, linha, coluna, lista, lista_borda):
     imagem[linha][coluna] = [255, 0, 0]
-    lista.append((linha, coluna))
+    lista.append((linha, coluna))  # Todos os pontos da ilha
     for i in range(linha - 1, linha + 2):
         for j in range(coluna - 1, coluna + 2):
             if i >= 0 and i <= len(imagem) - 1 and j >= 0 and j <= len(imagem[0]) - 1:  # Verifica se o ponto está dentro da matriz
-                if imagem[i][j][0] == 36 and imagem[i][j][1] == 231 and imagem[i][j][2] == 253:
+                if imagem[i][j][0] == 36 and imagem[i][j][1] == 231 and imagem[i][j][2] == 253:  # Amarelo
                     verifica_vizinhos(imagem, i, j, lista, lista_borda)
-                elif imagem[i][j][0] != 255 and imagem[i][j][1] != 0 and imagem[i][j][2] != 0 and (linha == i or coluna == j):
+                elif imagem[i][j][0] != 255 and imagem[i][j][1] != 0 and imagem[i][j][2] != 0 and (linha == i or coluna == j): 
                     lista_borda.append((linha, coluna))
 
 
 def encontra_b_box(lista_borda):
-    min_i = 500
+    min_i = 1e8
     max_i = 0
-    min_j = 500
+    min_j = 1e8
     max_j = 0
     for k in range(len(lista_borda)):
         linha = lista_borda[k][0]
@@ -38,6 +61,7 @@ def encontra_b_box(lista_borda):
     return [min_i, min_j, max_i, max_j]
 
 
+# Transformação linear para rotacionar os pontos
 def rotaciona_contorno(lista_borda, angulo):
     lista_rotacionada = []
     for i in range(len(lista_borda)):
@@ -48,15 +72,21 @@ def rotaciona_contorno(lista_borda, angulo):
     return lista_rotacionada
 
 
+# Função para segmentar a imagem em ilhas
 def segmentacao(imagem):
     n = 0
+    alt_pixel = metros_para_pixels(ALTURA, -22.328863, 18)
+    lar_pixel = metros_para_pixels(LARGURA, -22.328863, 18)
+
+    tam_painel = np.array([alt_pixel, lar_pixel])
+
     for i in range(len(imagem)):
         for j in range(len(imagem[0])):
             lista = []
             borda = []
-            if imagem[i][j][0] == 36 and imagem[i][j][1] == 231 and imagem[i][j][2] == 253:
+            if imagem[i][j][0] == 36 and imagem[i][j][1] == 231 and imagem[i][j][2] == 253:  # Amarelo
                 verifica_vizinhos(imagem, i, j, lista, borda)
-                if len(lista) < 300:
+                if len(lista) < 300:  # Retirar ilhas pequenas
                     continue
                 for k in range(len(lista)):
                     linha = lista[k][0]
@@ -65,15 +95,75 @@ def segmentacao(imagem):
                 n += 1
                 if n >= len(cores):
                     n = 0
-                borda_rotacionada = rotaciona_contorno(borda, np.pi/6)
-                b_box = encontra_b_box(borda_rotacionada)
-                pontos_extremos = [(b_box[0], b_box[1]), (b_box[0], b_box[3]), (b_box[2], b_box[3]), (b_box[2], b_box[1])]
-                extremos_rotacionados = rotaciona_contorno(pontos_extremos, -np.pi/6)
 
+                # Achar minima B_box
+                caixa = Retangulo(float("inf"))
+                for k in range(90):
+                    borda_rotacionada = rotaciona_contorno(borda, (np.pi * float(k))/180)  
+                    b_box = encontra_b_box(borda_rotacionada)
+                    area = (b_box[2] - b_box[0])*(b_box[3] - b_box[1])
+                    if area < caixa.area:
+                        caixa.area = area
+                        caixa.extremos = [(b_box[0], b_box[1]), (b_box[0], b_box[3]), (b_box[2], b_box[3]), (b_box[2], b_box[1])]
+                        caixa.angulo = (np.pi * float(k))/180
+                        caixa.lados = [(b_box[2] - b_box[0]), (b_box[3] - b_box[1])]
+                
+                extremos_rotacionados = rotaciona_contorno(caixa.extremos, -caixa.angulo)
+
+                # Desenha a caixa
                 plt.plot([extremos_rotacionados[0][1], extremos_rotacionados[1][1]], [extremos_rotacionados[0][0], extremos_rotacionados[1][0]], 'w')
                 plt.plot([extremos_rotacionados[1][1], extremos_rotacionados[2][1]], [extremos_rotacionados[1][0], extremos_rotacionados[2][0]], 'w')
                 plt.plot([extremos_rotacionados[2][1], extremos_rotacionados[3][1]], [extremos_rotacionados[2][0], extremos_rotacionados[3][0]], 'w')
                 plt.plot([extremos_rotacionados[3][1], extremos_rotacionados[0][1]], [extremos_rotacionados[3][0], extremos_rotacionados[0][0]], 'w')
+
+                eixos = rotaciona_contorno([[1, 0], [0, 1]], -caixa.angulo)
+                eixos = [np.array(e) for e in eixos]
+                ponto_inicial = np.array(extremos_rotacionados[0])
+                num_linhas = 0
+                l = 0
+                while True:
+                    l += 1
+                    desenha_painel(tam_painel, ponto_inicial, eixos[0], eixos[1])
+                    ponto_inicial += 1.2*tam_painel[0]*eixos[0]
+                    limite = ponto_inicial + tam_painel[0]*eixos[0]
+                    if limite[0]*eixos[0][0] + limite[1]*eixos[0][1] > caixa.lados[1]:
+                        num_linhas += 1
+                        ponto_inicial = np.array(extremos_rotacionados[0]) + num_linhas*1.5*tam_painel[1]*eixos[1]
+                        limite = ponto_inicial + tam_painel[1]*eixos[1]
+                        if limite[0]*eixos[1][0] + limite[1]*eixos[1][1] > caixa.lados[0]:
+                            break
+                print(f"rodou {l} vezes")
+                
+
+def desenha_painel(tam_painel_pixel, ponto_inicial, eixo_1, eixo_2):
+    p0 = ponto_inicial
+    p1 = eixo_1*tam_painel_pixel[0] + p0
+    p2 = eixo_2*tam_painel_pixel[1] + p0
+    p3 = p1 + p2 - p0
+    plt.plot([p0[1], p1[1]], [p0[0], p1[0]], 'g', linewidth=5)
+    plt.plot([p1[1], p3[1]], [p1[0], p3[0]], 'g', linewidth=5)
+    plt.plot([p3[1], p2[1]], [p3[0], p2[0]], 'g', linewidth=5)
+    plt.plot([p2[1], p0[1]], [p2[0], p0[0]], 'g', linewidth=5)
+
+
+# def coloca_paines(caixa):
+#     ponto_incial = caixa[0]
+#     primeiro_ponto = ponto_incial
+
+#     altura = np.array(caixa[1]) - np.array(ponto_incial)
+#     largura = np.array(caixa[3]) - np.array(ponto_incial)
+
+#     num_linhas = int(magnitude(altura))
+#     num_colunas = int(magnitude(largura))
+
+#     num_paineis = 0
+
+#     for i in range(num_colunas):
+#         for j in range(num_linhas):
+#             if j % 2 == 0:
+#                 cor = "red"
+#             else:
+#                 cor = "green"
 
 
 def main():
@@ -83,6 +173,9 @@ def main():
     segmentacao(imagem)
     plt.imshow(imagem)
     plt.show()
+
+    # lat = -22.328863	
+    # long = -49.1008054
 
     # roxo [84  1 68]
     # amarelo [ 36 231 253]
